672b4160049f8d6fa7edf322531a26f50198d305
amr <amrnawaresh@gmail.com>
2025-09-05 20:55:56 +0300

    edit user and delete account

---
+++ MyLMS2/ViewModels/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLMS2.ViewModels
+{
+    public class UserWithRolesViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Roles { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
