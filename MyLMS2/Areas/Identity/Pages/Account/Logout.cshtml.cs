74cdb365af5a5dc8d1cafad4854ccfa8328998d5
MohamedHamedrx <mohamedhamede564@gmail.com>
2025-09-05 18:56:38 +0300

    Update Logout and courses Front

---
+++ MyLMS2/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MyLMS2.Models;
+
+namespace MyLMS2.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        // عشان نوصل الـ ReturnUrl للـ View
+        [BindProperty]
+        public string ReturnUrl { get; set; }
+
+        public void OnGet()
+        {
+            // نجيب الصفحة اللي جه منها
+            ReturnUrl = HttpContext.Request.Headers["Referer"].ToString();
+
+            // fallback للهوم لو الـ Referer فاضي
+            if (string.IsNullOrEmpty(ReturnUrl))
+            {
+                ReturnUrl = Url.Page("/", new { area = "" });
+            }
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            // لو مفيش returnUrl نرجع للهوم
+            return RedirectToPage("/Index");
+        }
+    }
+}
