9c5ee22d3236c9da6bd7c53cf3f8f148263e9cac
Shady Tarek <shadytealab@gmail.com>
2025-09-06 11:46:10 +0300

    the enrollments page was added

---
+++ MyLMS2/Views/Enrollments/InstructorIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyLMS2.Views.Enrollments
+{
+    public class InstructorIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
